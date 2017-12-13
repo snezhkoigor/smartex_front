78fc70909f240e7a29994f09f20c2cf5d7e794eb
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-14 00:37:33 +0300

    add modules

--- src/api/axios_config.js
+++ src/api/axios_config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 let instance = axios.create({
-    baseURL: (process.env.NODE_ENV === 'production' ? 'http://api.smartex.com/api' : 'http://api.smartex/api'),
+    baseURL: (process.env.NODE_ENV === 'production' ? 'http://api.smartex.com/api' : 'http://smartexapi.vlevels.ru/api'),
     headers: {
         Authorization: 'Bearer ' + localStorage.getItem('jwt_token')
     },
