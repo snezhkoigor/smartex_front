15d054ac0c0edb1b764e8cb777e6793771d36dd7
igorsnezko <i.s.sergeevich@yandex.ru>
2017-12-14 23:07:12 +0300

    фиксированное меню при скроле

--- src/components/Layouts/Desktop.vue
+++ src/components/Layouts/Desktop.vue
@@ -1,8 +1,8 @@
 <template>
-	<v-container fluid grid-list-md class="hidden-md-and-down desktop-container">
+	<v-container fluid grid-list-md class="hidden-md-and-down desktop-container" v-scroll="onScroll">
 		<v-layout row wrap>
 			<v-flex d-flex d-flex xs2 sm2 class="flex-menu-container">
-				<v-card class="card--flex-toolbar">
+				<v-card :class="{'card--flex-toolbar': offsetTop <= 62, 'card--flex-toolbar fixed': offsetTop > 62}">
 					<menu-component />
 				</v-card>
 			</v-flex>
@@ -22,6 +22,17 @@
         components: {
             MenuComponent
         },
+        data () {
+            return {
+                offsetTop: 0
+            }
+        },
+
+        methods: {
+            onScroll (e) {
+                this.offsetTop = window.pageYOffset || document.documentElement.scrollTop
+            }
+        }
     }
 </script>
 
@@ -30,7 +41,13 @@
 		height: 100%;
 		width: 100%;
 	}
-
+	.flex-menu-container .fixed {
+		position: fixed;
+		z-index: 100;
+		top: 0;
+		margin-top: 5px;
+		margin-right: 4px;
+	}
 	.desktop-container {
 		max-width: 1200px;
 		padding: 0px 15px;
