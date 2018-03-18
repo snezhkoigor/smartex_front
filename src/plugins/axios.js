72aeac4d78e9ad56a395bc7898e297b17d2a329b
igorsnezko <i.s.sergeevich@yandex.ru>
2018-03-18 22:10:10 +0300

    добавление языка для новостей

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-axios.defaults.baseURL = (process.env.NODE_ENV === 'production' ? 'http://api.smartex.info/api' : 'http://api.smartex/api')
+axios.defaults.baseURL = (process.env.NODE_ENV === 'production' ? 'http://api.cryptocoinoficina.com/api' : 'http://api.smartex/api')
 axios.defaults.validateStatus = (status) => {
     return status >= 200 && status < 500
 }
